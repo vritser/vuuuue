1d25554862f513ddf5399215a46b202c4b7d9fb0
vritser <1429594204@qq.com>
2017-07-05 23:30:07 +0800

    something

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let $ = (tag, ...args) => args.reduce((a, b) => (b(a),a), document.createElement(tag))
+let $ = (tag, ...args) => args.reduce((a, b) => { b(a); return a }, document.createElement(tag))
 
 let text = t => n => n.innerText = t
 let append = d => n => d.appendChild(n)
+let on = (e, f) => n => n.addEventListener(e, f, false)
 
-$('h1', text('Hello World'), append(document.body))
+$('h1', on('click', x => alert(x.target.innerText)), text('Hello World'), append(document.body))
